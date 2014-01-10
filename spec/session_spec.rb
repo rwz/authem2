ba0b6bc730ab5f8735aaee2105a6dface290e75b
Lucas Rosa Galego and Pavel Pravosud <dev+lrgalego+rwz@hashrocket.com>
2014-01-10 14:57:30 -0500

    some session specs

--- spec/session_spec.rb
+++ spec/session_spec.rb
@@ -1,5 +1,13 @@
 require "spec_helper"
 
 describe Authem::Session do
+  let(:user){ User.create(email: "joe@example.com") }
+  let(:role){ :user }
+
+  it "generates secure token on creation" do
+    expect(SecureRandom).to receive(:hex).with(40).and_return("a secure token")
+    model = described_class.create(role: role, subject: user)
+    expect(model.token).to eq("a secure token")
+  end
 
 end
