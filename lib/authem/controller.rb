f80ae201fe8144f85300d7da5a7bdccf1668dd0e
Pavel Pravosud <pavel@pravosud.com>
2014-01-11 20:04:48 -0500

    use ruby 2 goodness

--- lib/authem/controller.rb
+++ lib/authem/controller.rb
@@ -8,24 +8,24 @@ module Authem
     included{ class_attribute :authem_settings }
 
     module SessionManagementMethods
-      def sign_in(model, options={})
+      def sign_in(model, **options)
         role = options.fetch(:as){ self.class.authem.role_for(model) }
         public_send "sign_in_#{role}", model
       end
 
-      def sign_out(model, options={})
+      def sign_out(model, **options)
         role = options.fetch(:as){ self.class.authem.role_for(model) }
         public_send "sign_out_#{role}"
       end
 
-      def clear_all_sessions_for(model, options={})
+      def clear_all_sessions_for(model, **options)
         role = options.fetch(:as){ self.class.authem.role_for(model) }
         public_send "clear_all_#{role}_sessions_for", model
       end
     end
 
     module ClassMethods
-      def authem_for(model_name, options={})
+      def authem_for(model_name, **options)
         include SessionManagementMethods
 
         authem.define_authem model_name, options
